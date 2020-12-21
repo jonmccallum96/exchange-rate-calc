fe07cfbf229cd9d9a24fd7896165c997cac61c2b
Jon <jon.a.mccallum@gmail.com>
2020-12-21 12:27:15 +0000

    declare DOM elements in js

---
+++ script.js
@@ -0,0 +1,7 @@
+const currencyEl1 = document.getElementById('currency-1');
+const amountEl1 = document.getElementById('amount-1');
+const currencyEl2 = document.getElementById('currency-2');
+const amountEl2 = document.getElementById('amount-2');
+
+const rateEl = document.getElementById('rate');
+const swapEl = document.getElementById('swap');
