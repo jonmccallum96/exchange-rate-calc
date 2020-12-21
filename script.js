ce20cb10c5f5642301a60248baf44fa915dfc426
Jon <jon.a.mccallum@gmail.com>
2020-12-21 13:03:40 +0000

    update available currency options

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ const amountEl2 = document.getElementById('amount-2');
 
 const rateEl = document.getElementById('rate');
 const swapEl = document.getElementById('swap');
+
+
+//FETCH EXCH RATES & UPDATE UI
+function calculate() {
+  const currency1 = currencyEl1.value;
+  const currency2 = currencyEl2.value;
+
+  fetch(`https://api.exchangeratesapi.io/latest?base=${currency1}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    });
+
+}
+
+//EVENT LISTENERS
+
+currencyEl1.addEventListener('change', calculate);
+amountEl1.addEventListener('input', calculate);
+currencyEl2.addEventListener('change', calculate);
+amountEl2.addEventListener('input', calculate);
+
+calculate();
